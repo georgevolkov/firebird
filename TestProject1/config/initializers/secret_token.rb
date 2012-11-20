637e747184653a139f0a723ed8cb682c7e85adda
georgevolkov <georgevolkov@gmail.com>
2012-11-20 20:52:01 -0300

    firt commit into branch shop

--- TestProject1/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TestProject1::Application.config.secret_token = 'eb110e076fcbe8835b3a3561045673fcdecc130b9e1919c8c8bd2e9420cdcfd1e4352bde42f653e973c4d4beebea555c9941f4aeca6fbbab04afc488f47779ca'
---
+++ shop/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = 'f92caace3a451884749a3a35ece9ec4620ba096c4204e9d0740005c2766c641808466e1fe0b213bfc42483b092ed29d8b8e945dba2666d3b37e8366008264ae9'
